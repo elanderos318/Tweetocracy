36354308b7db582291a4837c0594e878513ad60c
elanderos <elanderos318@gmail.com>
2019-07-06 21:13:53 -0500

    added materialize-css files

---
+++ static/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+// Initialization for sliding photos
+$(document).ready(function(){
+  $('.slider').slider();
+});
+     
